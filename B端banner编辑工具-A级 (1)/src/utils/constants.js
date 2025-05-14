b766e4d1a06e1703b4650d8744cdbafaba27e557
xm66er <2310751239@qq.com>
2025-05-14 17:45:08 +0800

    Add files via upload

---
+++ B端banner编辑工具-A级 (1)/src/utils/constants.js
@@ -0,0 +1,26 @@
+export const themes = {
+  yellow: {
+    name: 'yellow',
+    gradientStart: '#FFF5D9',
+    gradientEnd: '#FFFDF2',
+    highlightColor: '#FF8000',
+    buttonGradientStart: '#FF8C1A',
+    buttonGradientEnd: '#FF8000'
+  },
+  red: {
+    name: 'red',
+    gradientStart: '#FFDFD9',
+    gradientEnd: '#FFFFFF',
+    highlightColor: '#FF401A',
+    buttonGradientStart: '#FF4520',
+    buttonGradientEnd: '#FF6B4E'
+  },
+  blue: {
+    name: 'blue',
+    gradientStart: '#D7EAFF',
+    gradientEnd: '#FFFFFF',
+    highlightColor: '#1A66FF',
+    buttonGradientStart: '#1A66FF',
+    buttonGradientEnd: '#1A8CFF'
+  }
+};
